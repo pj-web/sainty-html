e36683f2321a40f7d312c739aaf803633284eb36
pj-web <insta.pj.05@gmail.com>
2021-07-19 22:56:08 +0300

    +s3

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+jQuery(function() {
     if ($('.music__slider-slider').length) {
         $('.music__slider-slider').slick({
             dots: false,
@@ -9,6 +9,18 @@ $(document).ready(function(){
             appendArrows: $('.music__header-flipping'),
             prevArrow: '<div><img src="images/arrow-left.svg"></div>',
             nextArrow: '<div><img src="images/arrow-right.svg"></div>'
+          });
+    }
+
+    if ($('.biography__image-slider').length) {
+        $('.biography__image-slider').slick({
+            dots: true,
+            arrows: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true,
+            
           });
     }
     

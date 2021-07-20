d2c5bde1c9e7227c4e3f995313b21f713c586eec
pj-web <insta.pj.05@gmail.com>
2021-07-20 20:08:50 +0300

    generals-done

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,20 @@
+window.addEventListener('DOMContentLoaded', function () {
+    let videos = document.querySelectorAll('.music__slider-item');
+
+    videos.forEach(function(video) {
+        video.addEventListener('click', function() {
+            video.classList.add('ready');
+            if (video.classList.contains('ready')) {
+                return;
+            }
+
+            let src = video.dataset.src;
+
+            video.insertAdjacentHTML('afterbegin', '<iframe src="' + src + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+        });
+    });
+});
+
 jQuery(function() {
     if ($('.music__slider-slider').length) {
         $('.music__slider-slider').slick({
